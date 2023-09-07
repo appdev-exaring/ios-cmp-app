c9b51eb96c0bae7dd95524ed76ceac3a4e054529
Reiner Pittinger <reiner.pittinger@exaring.de>
2023-09-07 16:04:49 +0200

    First effort to add tvOS support to SPM

--- Package.swift
+++ Package.swift
@@ -4,28 +4,44 @@ import PackageDescription
 let package = Package(
   name: "ConsentViewController",
   platforms: [
-    .iOS(.v10)
+    .iOS(.v11),
+    .tvOS(.v14)
   ],
   products: [
     .library(
       name: "ConsentViewController",
-      targets: ["ConsentViewController"]),
+      targets: [
+        "ConsentViewController",
+        "ConsentViewController-iOS",
+        "ConsentViewController-tvOS"
+      ]),
   ],
-  dependencies: [],
+  dependencies: [
+    .package(url: "https://github.com/johnxnguyen/Down", from: "0.11.0"),
+  ],
   targets: [
     .target(
       name: "ConsentViewController",
-      dependencies: [],
       path: "ConsentViewController",
       exclude: [
         "Assets/javascript/SPJSReceiver.spec.js",
         "Assets/javascript/jest.config.json",
-        "Classes/Views/tvOS/",
+        "Classes/Views"
       ],
       resources: [
         .process("Assets/javascript/SPJSReceiver.js"),
         .process("Assets/images")
       ]
+    ),
+    .target(
+      name: "ConsentViewController-iOS",
+      dependencies: ["ConsentViewController"],
+      path: "ConsentViewController/Classes/Views/iOS"
+    ),
+    .target(
+      name: "ConsentViewController-tvOS",
+      dependencies: ["Down", "ConsentViewController"],
+      path: "ConsentViewController/Classes/Views/tvOS"
     )
   ],
   swiftLanguageVersions: [.v5]
