6c05e84c478fc4994b7e256e0da41b282109c161
Reiner Pittinger <reiner.pittinger@exaring.de>
2023-09-12 18:04:48 +0200

    Fixed bundle path for tvOS

--- ConsentViewController/Classes/Bundle+Framework.swift
+++ ConsentViewController/Classes/Bundle+Framework.swift
@@ -16,7 +16,7 @@ extension Foundation.Bundle {
         #if SWIFT_PACKAGE
 
         #if os(tvOS)
-        return Bundle(for: ConsentViewController_tvOS.self)
+        return Bundle(url: Bundle.main.bundleURL.appending(path: "ConsentViewController-tvOSResources_ConsentViewControllerResources.bundle"))!
         #else
         return Bundle.module
         #endif
